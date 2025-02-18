f6840fff4685ca013c676b14a8247a6dc68a7b51
Altik01 <altik8650@gmail.com>
2025-02-18 21:54:31 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-$(document).ready(function() {
-    let hiddenRow = $(".expandable");
-    $(".expand-btn").click(function () {
-      hiddenRow.toggleClass("hidden");
-      if (hiddenRow.is(":visible")) { // is this element is visible
-        $(".expand-btn").html("put away");
-      }
-      else {
-        $(".expand-btn").html("Put away");
-      }
-    });
-    $(".play-btn").click( function () {
-      $(".modal-conves").toggleClass("hidden");
-    }); 
-    $(".modal-close").click( function () {
-      $(".modal-conves").addClass("hidden");
-    });
-  });
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".btn").addEventListener("click", function() {
+        alert("Welcome to EduMaster!");
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.querySelector(".menu-btn");
+    const navMenu = document.querySelector("nav ul");
+
+    menuBtn.addEventListener("click", function () {
+        navMenu.style.display = navMenu.style.display === "flex" ? "none" : "flex";
+    });
+});
